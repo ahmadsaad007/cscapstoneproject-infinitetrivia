1a9511813f00344fea1d45d4b2490cfe38c4011e
Ahmad Saad <ahmadsd97@gmail.com>
2020-03-29 03:51:20 -0400

    First Draft of SVM Classifier

--- trivia_generator/TUnit.py
+++ trivia_generator/TUnit.py
@@ -9,7 +9,6 @@ from dataclasses import dataclass
 @dataclass
 class TUnit:
     """Representation of a trivia unit and its associated metadata.
-
     :param sentence: the original sentence from Wikipedia.
     :type sentence: str
     :param article_id: the ID of the Wikipedia article.
@@ -20,18 +19,16 @@ class TUnit:
     :type categories: List[str]
     :param access_timestamp: the Unix timestamp at which the article was accessed by the WebScraper.
     :type access_timestamp: int
-
     :param has_superlative: a boolean value representing whether the sentence has a superlative in it.
-    :type has_superlative: bool
+    :type has_superlative: int
     :param has_contrasting: a boolean value representing whether the sentence has a contrasting word in it.
-    :type has_contrasting: bool
+    :type has_contrasting: int
     :param root_word: the root word of the sentence.
     :type root_word: str
     :param subj_word: the subject of the sentence.
     :type subj_word: str
     :param readability: the Gunning FOG index of sentence (see https://www.readabilityformulas.com/gunning-fog-readability-formula.php)
     :type readability: int
-
     :param t_unit_id: the auto-assigned id of the TUnit in the database
     :type t_unit_id: int
     :param longitude: the longitude of the physical location of the article, if relevant.
@@ -48,13 +45,10 @@ class TUnit:
     article_id: int
     url: str
 
-    categories: list
     access_timestamp: int
 
-    has_superlative: bool
-    has_contrasting: bool
-    root_word: str
-    subj_word: str
+    has_superlative: int
+    has_contrasting: int
     readability: int
 
     t_unit_id: int = None
@@ -62,23 +56,44 @@ class TUnit:
     latitude: float = None
     longitude: float = None
 
-    trivia_rank: int = None
-    question_rank: int = None
+    num_likes: int = 0
+    num_mehs: int = 0
+    num_dislikes: int = 0
+
+
+    def to_tsv_line(self) -> str:
+        return '\t'.join([str(attribute) for attribute in
+            [
+                self.sentence,
+                self.article_id,
+                self.url,
+                self.access_timestamp,
+                self.has_superlative,
+                self.has_contrasting,
+                self.readability,
+                self.t_unit_id,
+                self.latitude,
+                self.longitude,
+                self.num_likes,
+                self.num_mehs,
+                self.num_dislikes
+            ]
+        ])
 
     def __str__(self):
-        categories_str = '\n      - '.join(self.categories)
         return f"""TUnit:
   - Sentence: '{self.sentence[:137]}...'
   - Article ID: {self.article_id}
   - URL: {self.url}
-  - Categories:
-      - {categories_str}
   - Access Timestamp: {self.access_timestamp}
   - Coordinates:  ({self.latitude}, {self.longitude})
   - Has Superlative: {self.has_superlative}
   - Has Contrasting Phrases: {self.has_contrasting}
-  - Root Word: {self.root_word}
-  - Subject Word: {self.subj_word}
   - Readability (FOG): {self.readability}
   - Trivia Rank: {self.trivia_rank}
   - Question Rank: {self.question_rank}"""
+
+def tunit_list_to_tsv(tunits: list, output_filename = 'tunits.tsv'):
+    csv_output = '\n'.join([tunit.to_tsv_line() for tunit in tunits]) + '\n'
+    with open(output_filename, 'a+') as f:
+        f.write(csv_output)
