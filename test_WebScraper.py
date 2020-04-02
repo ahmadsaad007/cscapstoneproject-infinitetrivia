98e99d7019fba8708397cacc59b7e4be01eeef22
coizioc <36041795+coizioc@users.noreply.github.com>
2020-04-02 15:33:22 -0400

    Add citation remover in Web Scraper.

--- test_WebScraper.py
+++ test_WebScraper.py
@@ -49,3 +49,19 @@ def test_convert_dms_to_decimal():
     for dms, decimal in dms_test_suite:
         test_decimal = round(convert_dms_to_decimal(dms), 3)
         assert decimal == test_decimal
+
+def test_remove_citations():
+    original_expected_texts = [
+        (
+            """The American City Business Journals, which conducts a regular study to determine the most loyal fans in the NFL, evaluates fans based primarily on attendance-related factors,[82] and ranked Eagles fans third in both 1999[83] and 2006.[84] The 2006 study called the fans "incredibly loyal", noting that they filled 99.8% of the seats in the stadium over the previous decade.[85] Forbes placed the Eagles fans first in its 2008 survey,[86] which was based on the correlation between team performance and fan attendance.[87] ESPN.com placed Eagles fans fourth in the league in its 2008 survey, citing the connection between the team's performance and the mood of the city.[88] The last home game that was blacked out on television in the Philadelphia market as a result of not being sold out was against the Arizona Cardinals on Sunday, September 12, 1999, which was Andy Reid's first home game as new head coach of the Eagles.[citation needed]""",
+            """The American City Business Journals, which conducts a regular study to determine the most loyal fans in the NFL, evaluates fans based primarily on attendance-related factors, and ranked Eagles fans third in both 1999 and 2006. The 2006 study called the fans "incredibly loyal", noting that they filled 99.8% of the seats in the stadium over the previous decade. Forbes placed the Eagles fans first in its 2008 survey, which was based on the correlation between team performance and fan attendance. ESPN.com placed Eagles fans fourth in the league in its 2008 survey, citing the connection between the team's performance and the mood of the city."""
+        ),
+        (
+            """The name "Guinea" is of uncertain origin.[23] Residents in this area have been referred to as "Guineamen" at least since 1730, according to a tombstone inscription found by Brewton Berry (1963).[citation needed] As noted by George Dow in 1969, London physician George Pinckard referred to the master of a ship containing slaves from the Guinea coast as a "Guinea Man" in letters dating 1795.[citation needed] It is likely this area was called Guinea after being used as a landing site for importation of slaves from that area. Another story[2], passed among the "Guineamen" is that people on the Guinea Neck were continuing to use golden Guineas to pay for things up from about 1781–1860, the start of the American Civil War.[note 1][6]""",
+            """The name "Guinea" is of uncertain origin. It is likely this area was called Guinea after being used as a landing site for importation of slaves from that area. Another story, passed among the "Guineamen" is that people on the Guinea Neck were continuing to use golden Guineas to pay for things up from about 1781–1860, the start of the American Civil War."""
+        )
+    ]
+
+    for original, expected in original_expected_texts:
+        processed_text = remove_citations(original)
+        assert processed_text == expected
