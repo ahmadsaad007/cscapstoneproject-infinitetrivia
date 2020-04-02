5bfc96b06a939460b9ef667cbed2a8a7865e50b4
coizioc <36041795+coizioc@users.noreply.github.com>
2020-04-02 15:33:55 -0400

    Move content of contradictatory.txt to Contradictatory Matcher.

--- nlp_helpers/ContradictatoryMatcher.py
+++ nlp_helpers/ContradictatoryMatcher.py
@@ -1,11 +1,46 @@
 from spacy.matcher import PhraseMatcher
 
-import os
-
 from nlp_helpers import NLPConn
 
-TOP_LEVEL_DIR = os.path.abspath('../')
-contradictory_path = TOP_LEVEL_DIR + '/contradictatory.txt'
+contradictatory_phrases = [
+    'although',
+    'in contrast',
+    'different from',
+    'of course',
+    'on the other hand',
+    'on the contrary',
+    'at the same time',
+    'in spite of',
+    'even so',
+    'though',
+    'be that as it may',
+    'then again',
+    'above all',
+    'in reality',
+    'after all',
+    'but',
+    'still',
+    'unlike',
+    'or',
+    'yet',
+    'while',
+    'albeit',
+    'besides',
+    'as much as',
+    'even though',
+    'although',
+    'instead',
+    'whereas',
+    'despite',
+    'conversely',
+    'otherwise',
+    'however',
+    'rather',
+    'nevertheless',
+    'nonetheless',
+    'regardless',
+    'notwithstanding'
+]
 
 matcher = None
 
@@ -13,9 +48,7 @@ def get_contradicatory_matcher():
     global matcher
     if matcher is None:
         nlp = NLPConn.get_nlp_conn()
-        with open(contradictory_path, 'r') as f:
-            contradictatory_words = f.read().splitlines()
-            matcher = PhraseMatcher(nlp.vocab)
-            patterns = [nlp.make_doc(text) for text in contradictatory_words]
-            matcher.add("ContradictoryTokens", None, *patterns)
+        matcher = PhraseMatcher(nlp.vocab)
+        patterns = [nlp.make_doc(phrase) for phrase in contradictatory_phrases]
+        matcher.add("ContradictoryTokens", None, *patterns)
     return matcher
